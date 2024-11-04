294d589da817f7544d00bd207c55d59262409c0a
fionnferreira <fionnferreira@gmail.com>
2024-11-04 14:33:12 +0100

    Fix: monitor rpm

--- gpio_test.py
+++ gpio_test.py
@@ -2,30 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin configuration
-led_pin = 18  # GPIO pin for the LED
-button_pin = 23  # GPIO pin for the button
+pump_pin = 18  # GPIO pin for the pump control
+speed_pin = 23  # GPIO pin for the pump speed input
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pump_pin, GPIO.OUT)
+GPIO.setup(speed_pin, GPIO.IN)
 
 try:
     while True:
-        # Read the button state
-        button_state = GPIO.input(button_pin)
+        # Set the pump control pin to HIGH (5V)
+        GPIO.output(pump_pin, GPIO.HIGH)
+        print("Pump running")
 
-        if button_state == GPIO.LOW:
-            # Button is pressed, turn on the LED
-            GPIO.output(led_pin, GPIO.HIGH)
-            print("Button pressed, LED on")
-        else:
-            # Button is not pressed, turn off the LED
-            GPIO.output(led_pin, GPIO.LOW)
-            print("Button not pressed, LED off")
+        # Read the value of the speed pin
+        speed_value = GPIO.input(speed_pin)
+        print(f"Speed pin value: {speed_value}")
 
-        # Wait for a short period to debounce the button
-        time.sleep(0.1)
+        # Wait for a short period before measuring again
+        time.sleep(1)
 
 except KeyboardInterrupt:
     pass  # Exit cleanly on Ctrl+C
